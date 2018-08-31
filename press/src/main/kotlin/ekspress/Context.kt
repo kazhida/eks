a501903a438d9b87ba4a017019ed38ce66a8994f
kazhida <kazhida@abplus.com>
2018-08-31 14:14:34 +0900

    ステータスコード毎にメソッドを用意した。何を返すべきかは後で考える。

--- press/src/main/kotlin/ekspress/Context.kt
+++ press/src/main/kotlin/ekspress/Context.kt
@@ -42,10 +42,13 @@ data class Context(
 
     /**
      * 空のレスポンス
-     * （Application.ktでのみ使用する）
+     * （ekspressパッケージ内でのみ使用する）
      */
-    internal fun endAsEmpty() = copy(
+    internal fun endAsEmpty(status: Int? = null) = copy(
             response = response.copy().apply {
+                if (status != null) {
+                    res.status = status
+                }
                 res.body = null
                 res.end().unsafeCast<Unit>()
             },
@@ -54,7 +57,7 @@ data class Context(
 
     /**
      * HEADメソッドに対応するレスポンス
-     * （Application.ktでのみ使用する）
+     * （ekspressパッケージ内でのみ使用する）
      */
     internal fun endAsHead() = copy(
             response = response.copy().apply {
@@ -69,7 +72,7 @@ data class Context(
     /**
      * ステータスコードをボディにするレスポンス
      * ボディが空のときに使用
-     * （Application.ktでのみ使用する）
+     * （ekspressパッケージ内でのみ使用する）
      */
     internal fun endAsCode() = copy(
             response = response.copy().apply {
@@ -85,7 +88,7 @@ data class Context(
 
     /**
      * いわゆるNotFoundのレスポンス
-     * （Application.ktでのみ使用する）
+     * （ekspressパッケージ内でのみ使用する）
      */
     internal fun endAsNotFound() = copy(
             response = this.response.copy().apply {
@@ -96,85 +99,24 @@ data class Context(
 
     /**
      * 通常のレスポンス
-     * （Application.ktでのみ使用する）
+     * （ekspressパッケージ内でのみ使用する）
      */
-    internal fun endAsNormally() = copy(
+    internal fun endAsNormally(status: Int? = null, type: String? = null, body: String? = null) = copy(
             response = this.response.copy().apply {
                 // todo: bodyがStreamのときのことは考慮していない
+                if (status != null) {
+                    res.status = status
+                }
+                if (type != null) {
+                    res.type = type
+                }
+                if (body != null) {
+                    res.body = body
+                }
                 res.length = res.body.length
             },
             isStopped = true
     )
-
-    @Suppress("unused")
-    private fun isValidStatus(status: Int): Boolean = when (status) {
-        100, //"Continue"),
-        101, //"Switching Protocols"),
-        102, //"Processing"),
-        103, //"Early Hints"),
-        200, //"OK"),
-        201, //"Created"),
-        202, //"Accepted"),
-        203, //"Non-Authoritative Information"),
-        204, //"No Content"),
-        205, //"Reset Content"),
-        206, //"Partial Content"),
-        207, //"Multi-Status"),
-        208, //"Already Reported"),
-        226, //"IM Used"),
-        300, //"Multiple Choices"),
-        301, //"Moved Permanently"),
-        302, //"Found"),
-        303, //"See Other"),
-        304, //"Not Modified"),
-        305, //"Use Proxy"),
-        306, //"(Unused)"),
-        307, //"Temporary Redirect"),
-        308, //"Permanent Redirect"),
-        400, //"Bad Request"),
-        401, //"Unauthorized"),
-        402, //"Payment Required"),
-        403, //"Forbidden"),
-        404, //"Not Found"),
-        405, //"Method Not Allowed"),
-        406, //"Not Acceptable"),
-        407, //"Proxy Authentication Required"),
-        408, //"Request Timeout"),
-        409, //"Conflict"),
-        410, //"Gone"),
-        411, //"Length Required"),
-        412, //"Precondition Failed"),
-        413, //"Payload Too Large"),
-        414, //"URI Too Long"),
-        415, //"Unsupported Media Type"),
-        416, //"Range Not Satisfiable"),
-        417, //"Expectation Failed"),
-        418, //"I'm a teapot"),
-        421, //"Misdirected Request"),
-        422, //"Unprocessable Entity"),
-        423, //"Locked"),
-        424, //"Failed Dependency"),
-        425, //"Unordered Collection"),
-        426, //"Upgrade Required"),
-        428, //"Precondition Required"),
-        429, //"Too Many Requests"),
-        431, //"Request Header Fields Too Large"),
-        451, //"Unavailable For Legal Reasons"),
-        500, //"Internal NodeServer Error"),
-        501, //"Not Implemented"),
-        502, //"Bad Gateway"),
-        503, //"Service Unavailable"),
-        504, //"Gateway Timeout"),
-        505, //"HTTP Version Not Supported"),
-        506, //"Variant Also Negotiates"),
-        507, //"Insufficient Storage"),
-        508, //"Loop Detected"),
-        509, //"Bandwidth Limit Exceeded"),
-        510, //"Not Extended"),
-        511, //"Network Authentication Required")
-        999 -> true
-        else -> false
-    }
 
     /**
      * リクエスト
@@ -238,4 +180,205 @@ data class Context(
          */
         val body: String? get() = res.body.unsafeCast<String?>()
     }
+    
+    /**
+     * どうしたらいいかわからないステータスのときは、
+     * ステータスだけ設定しておく
+     */
+
+    @Suppress("unused")
+    fun continue100(): Context = endAsNormally(100)
+    
+    @Suppress("unused")
+    fun switchingProtocols101() = endAsNormally(101)
+    
+    @Suppress("unused")
+    fun processing102() = endAsNormally(102)
+    
+    @Suppress("unused")
+    fun earlyHints103() = endAsNormally(1003)
+    
+    @Suppress("unused")
+    fun ok200(type: String, body: String) = endAsNormally(200, type, body) //"OK"),
+    
+    @Suppress("unused")
+    fun created201() = endAsNormally(201)
+    
+    @Suppress("unused")
+    fun accepted202() = endAsNormally(202)
+    
+    @Suppress("unused")
+    fun nonAuthoritativeInformation203() = endAsNormally(203)
+    
+    @Suppress("unused")
+    fun noContent204() = endAsEmpty(204)
+    
+    @Suppress("unused")
+    fun resetContent205() = endAsEmpty(205)
+    
+    @Suppress("unused")
+    fun partialContent206() = endAsNormally(203)
+    
+    @Suppress("unused")
+    fun multiStatus207() = endAsNormally(203)
+    
+    @Suppress("unused")
+    fun alreadyReported208() = endAsNormally(203)
+    
+    @Suppress("unused")
+    fun imUsed226() = endAsNormally(226)
+    
+    @Suppress("unused")
+    fun multipleChoices300() = endAsNormally(300)
+    
+    @Suppress("unused")
+    fun movedPermanently301() = endAsNormally(301)
+    
+    @Suppress("unused")
+    fun found302() = endAsNormally(302)
+
+    @Suppress("unused")
+    fun seeOther303() = endAsNormally(303)
+
+    @Suppress("unused")
+    fun notModified304() = endAsEmpty(304)
+
+    @Suppress("unused")
+    fun useProxy305() = endAsNormally(305)
+
+    @Suppress("unused")
+    fun unused306() = endAsNormally(306)
+
+    @Suppress("unused")
+    fun temporaryRedirect307() = endAsNormally(307)
+
+    @Suppress("unused")
+    fun permanentRedirect308() = endAsNormally(308)
+
+    @Suppress("unused")
+    fun badRequest400() = endAsNormally(400)
+
+    @Suppress("unused")
+    fun unauthorized401() = endAsNormally(401)
+
+    @Suppress("unused")
+    fun paymentRequired402() = endAsNormally(402)
+
+    @Suppress("unused")
+    fun forbidden403() = endAsNormally(403)
+
+    @Suppress("unused")
+    fun notFound404() = endAsNormally(404)
+
+    @Suppress("unused")
+    fun methodNotAllowed405() = endAsNormally(405)
+
+    @Suppress("unused")
+    fun notAcceptable406() = endAsNormally(406)
+
+    @Suppress("unused")
+    fun proxyAuthenticationRequired407() = endAsNormally(407)
+
+    @Suppress("unused")
+    fun requestTimeout408() = endAsNormally(408)
+
+    @Suppress("unused")
+    fun conflict409() = endAsNormally(409)
+
+    @Suppress("unused")
+    fun gone410() = endAsNormally(410)
+
+    @Suppress("unused")
+    fun lengthRequired411() = endAsNormally(411)
+
+    @Suppress("unused")
+    fun preconditionFailed412() = endAsNormally(412)
+
+    @Suppress("unused")
+    fun payloadTooLarge413() = endAsNormally(413)
+
+    @Suppress("unused")
+    fun uriTooLong414() = endAsNormally(414)
+
+    @Suppress("unused")
+    fun unsupportedMediaType415() = endAsNormally(415)
+
+    @Suppress("unused")
+    fun rangeNotSatisfiable416() = endAsNormally(416)
+
+    @Suppress("unused")
+    fun expectationFailed417() = endAsNormally(417)
+
+    @Suppress("unused")
+    fun imaTeapot418() = endAsNormally(418)
+
+    @Suppress("unused")
+    fun misdirectedRequest421() = endAsNormally(421)
+
+    @Suppress("unused")
+    fun unprocessableEntity422() = endAsNormally(422)
+
+    @Suppress("unused")
+    fun locked423() = endAsNormally(423)
+
+    @Suppress("unused")
+    fun failedDependency424() = endAsNormally(424)
+
+    @Suppress("unused")
+    fun unorderedCollection425() = endAsNormally(425)
+
+    @Suppress("unused")
+    fun upgradeRequired426() = endAsNormally(426)
+
+    @Suppress("unused")
+    fun preconditionRequired428() = endAsNormally(428)
+
+    @Suppress("unused")
+    fun tooManyRequests429() = endAsNormally(429)
+
+    @Suppress("unused")
+    fun requestHeaderFieldsTooLarge431() = endAsNormally(431)
+
+    @Suppress("unused")
+    fun unavailableForLegalReasons451() = endAsNormally(451)
+
+    @Suppress("unused")
+    fun internalServerError500() = endAsNormally(500)
+
+    @Suppress("unused")
+    fun notImplemented501() = endAsNormally(501)
+
+    @Suppress("unused")
+    fun badGateway502() = endAsNormally(502)
+
+    @Suppress("unused")
+    fun serviceUnavailable503() = endAsNormally(503)
+
+    @Suppress("unused")
+    fun gatewayTimeout504() = endAsNormally(504)
+
+    @Suppress("unused")
+    fun httpVersionNotSupported505() = endAsNormally(505)
+
+    @Suppress("unused")
+    fun variantAlsoNegotiates506() = endAsNormally(506)
+
+    @Suppress("unused")
+    fun insufficientStorage507() = endAsNormally(507)
+
+    @Suppress("unused")
+    fun loopDetected508() = endAsNormally(508)
+
+    @Suppress("unused")
+    fun bandwidthLimitExceeded509() = endAsNormally(509)
+
+    @Suppress("unused")
+    fun notExtended510() = endAsNormally(510)
+
+    @Suppress("unused")
+    fun networkAuthenticationRequired511() = endAsNormally(511)
+
+    // fixme toJsonが欲しいところ
+    @Suppress("unused")
+    fun okJson(body: Any): Context = ok200("text/json", body.toString())
 }
